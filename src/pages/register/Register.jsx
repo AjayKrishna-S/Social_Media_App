601702982acc80ac62344a60567bcb8d43c98a3e
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-03-11 22:42:02 +0530

    register page scss completed

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,26 @@ import './register.scss'
 
 const Register = () => {
   return (
-    <div className='register'>Register</div>
+    <div className='register'>
+      <div className="card">
+        <div className="left">
+          <h1>MeetUp.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora porro aliquid aspernatur ratione sequi maiores repellat iste. Numquam.</p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type='text' placeholder='Username'></input>
+            <input type='mail' placeholder='Email'></input>
+            <input type='password' placeholder='Password'></input>
+            <input type='text' placeholder='Name'></input>
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 

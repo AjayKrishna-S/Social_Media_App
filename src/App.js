4b2902a706ff8822bb03c6fe3cd1468c72fd146c
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-03-12 13:52:57 +0530

    created component layouts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import LeftBar from "./components/leftBar/LeftBar";
+import NavBar from "./components/navBar/NavBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import { createBrowserRouter, RouterProvider, Outlet, Route, Navigate, ProductedRoute } from "react-router-dom";
 
 function App() {
+
+  const currentUser = true
+
+  const Layout = ()=>{
+    return(
+       <div>
+        <NavBar />
+        <div style={{display:"flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    )
+  }
+
+  const ProductedRoute = ({children}) =>{
+    if(!currentUser){
+      return <Navigate to="/login" />
+    }
+    return children
+  }
+
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: 
+        <ProductedRoute>
+          <Layout />
+        </ProductedRoute>
+      ,
+      children:[
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />
+        }
+      ]
+    },
     {
       path: "/login",
       element: <Login />,
---
+++ src/components/leftBar/LeftBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './leftBar.scss';
+
+const LeftBar = () => {
+  return (
+    <div className='leftB ar'>LeftBar</div>
+  )
+}
+
+export default LeftBar

5dc705c282fc11e9f055ecab982ae68c5ebae558
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-03-12 14:51:59 +0530

    scss for nav bar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import './navBar.scss';
+import { Link } from 'react-router-dom';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 
 const NavBar = () => {
   return (
-    <div className='navBar'>NavBar</div>
+    <div className='navBar'>
+        <div className='left'>
+            <Link to='/' style={{textDecoration:"none"}}>
+                <span>MeetUp</span>
+            </Link>
+            <HomeOutlinedIcon />
+            <DarkModeOutlinedIcon />
+            <GridViewOutlinedIcon />
+            <div className='search'>
+                <SearchOutlinedIcon />
+                <input type='text' placeholder='Search'/>
+            </div>
+        </div>
+        <div className='right'>
+            <PersonOutlineOutlinedIcon />
+            <MailOutlinedIcon />
+            <NotificationsOutlinedIcon />
+            <div className='user'>
+                <img src="https://images.pexels.com/photos/1549280/pexels-photo-1549280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+                <span>Ajay Krishna</span>
+            </div>
+        </div>
+    </div>
   )
 }
 

ff6c51be0841a5f2af69de525e8e5240cd1f6bc3
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-03-13 17:32:59 +0530

    complete right bar scss

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -3,7 +3,89 @@ import './rightBar.scss'
 
 const RightBar = () => {
   return (
-    <div className='rightBar'>RightBar</div>
+    <div className='rightBar'>
+      <div className="container">
+        <div className="item">
+          <span>Suggestions for you</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src='https://images.pexels.com/photos/709188/pexels-photo-709188.jpeg?auto=compress&cs=tinysrgb&w=600' />
+              <span>mark zuck</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src='https://images.pexels.com/photos/709188/pexels-photo-709188.jpeg?auto=compress&cs=tinysrgb&w=600' />
+              <span>mark zuck</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src='https://images.pexels.com/photos/709188/pexels-photo-709188.jpeg?auto=compress&cs=tinysrgb&w=600' />
+              <p><span>mark zuck</span> changed their cover picture</p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src='https://images.pexels.com/photos/709188/pexels-photo-709188.jpeg?auto=compress&cs=tinysrgb&w=600' />
+              <p><span>mark zuck</span> changed their cover picture</p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src='https://images.pexels.com/photos/709188/pexels-photo-709188.jpeg?auto=compress&cs=tinysrgb&w=600' />
+              <p><span>mark zuck</span> changed their cover picture</p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src='https://images.pexels.com/photos/709188/pexels-photo-709188.jpeg?auto=compress&cs=tinysrgb&w=600' />
+              <p><span>mark zuck</span> changed their cover picture</p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src='https://images.pexels.com/photos/709188/pexels-photo-709188.jpeg?auto=compress&cs=tinysrgb&w=600' />
+              <div className='online' />
+              <span>mark zuck</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src='https://images.pexels.com/photos/709188/pexels-photo-709188.jpeg?auto=compress&cs=tinysrgb&w=600' />
+              <div className='online'></div>
+              <span>mark zuck</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src='https://images.pexels.com/photos/709188/pexels-photo-709188.jpeg?auto=compress&cs=tinysrgb&w=600' />
+              <div className='online'></div>
+              <span>mark zuck</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

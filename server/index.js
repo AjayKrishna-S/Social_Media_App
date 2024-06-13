506efe174a5ea90c657906cb007c4830d053b57e
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-06-13 11:48:43 +0530

    set router

---
+++ server/index.js
@@ -0,0 +1,10 @@
+// const Express = require('express');
+import Express from 'express';
+import userRoutes from './routes/users.js'
+const app = Express();
+
+app.use('/api/users',userRoutes);
+
+app.listen(8800,()=>{
+    console.log("API Working");
+})

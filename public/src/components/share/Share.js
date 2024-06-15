adc2a54386d6ddd8618e928a938105023fce73cf
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-06-16 01:02:56 +0530

    create post with using mutation

--- public/src/components/share/Share.js
+++ public/src/components/share/Share.js
@@ -1,41 +1,62 @@
 import './share.scss';
+import { makeRequest } from "../../axios.js";
 import Upload from "../../assets/upload.png";
 import Location from "../../assets/add-location.png";
 import Tag from "../../assets/tag-friend.png";
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../context/authContext';
+import { useQueryClient,useMutation } from '@tanstack/react-query';
 
 const Share = () => {
     const { currentUser } = useContext(AuthContext);
+
+    const queryClient = useQueryClient(); 
+
+    const [file, setFile] = useState(null);
+    const [desc, setDesc] = useState(null);
+
+    const mutation = useMutation({
+        mutationFn: (newPost) => {
+            return makeRequest.post('/posts', newPost);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries(["posts"]);
+        }
+    });
+
+    const handleSubmit = (e) =>{
+        e.preventDefault(); 
+        mutation.mutate({desc})
+    };
 
   return (
     <div className='share'>
         <div className="container">
             <div className="top">
                 <img src={currentUser.profilePic} alt="profilePic" />
-                <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} />
+                <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} onChange={e => setDesc(e.target.value)} />
             </div>
             <hr />
-        </div>
-        <div className="bottom">
-            <div className="left">
-                <input type="file" id="file" style={{display:"none"}} />
-                <label htmlFor="file">
-                    <div className='item'>
-                        <img src={Upload} alt="upload" />
-                        <span>Add Image</span>
+            <div className="bottom">
+                <div className="left">
+                    <input type="file" id="file" style={{display:"none"}} onChange={e => setFile(e.target.value)}/>
+                    <label htmlFor="file">
+                        <div className='item'>
+                            <img src={Upload} alt="upload" />
+                            <span>Add Image</span>
+                        </div>
+                    </label>
+                    <div className="item">
+                        <img src={Location} alt="map" />
+                        <span>Add Place</span>
                     </div>
-                </label>
-                <div className="item">
-                    <img src={Location} alt="map" />
-                    <span>Add Place</span>
-                </div>
-                <div className='item'>
-                    <img src={Tag} alt="Tag" />
-                    <span>Tag Friends</span>
+                    <div className='item'>
+                        <img src={Tag} alt="Tag" />
+                        <span>Tag Friends</span>
+                    </div>
                 </div>
                 <div className="right">
-                    <button>Share</button>
+                    <button onClick={handleSubmit}>Share</button>
                 </div>
             </div>
         </div>
